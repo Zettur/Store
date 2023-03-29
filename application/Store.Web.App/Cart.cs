c7a85e6a4962e01636bcc2a1a3e8e10158050518
Kraken989 <mrselfishguy@gmail.com>
2023-03-29 17:09:38 +0300

    Add project files.

---
+++ application/Store.Web.App/Cart.cs
@@ -0,0 +1,21 @@
+﻿namespace Store.Web.App
+{
+    public class Cart
+    {
+        public int OrderId { get; }
+
+        public int TotalCount { get; }
+
+        public decimal TotalPrice { get; }
+
+        public string TotalPriceS { get; }
+
+        public Cart(int orderId, int totalCount, decimal totalPrice)
+        {
+            OrderId = orderId;
+            TotalCount = totalCount;
+            TotalPrice = totalPrice;
+            TotalPriceS = totalPrice.ToString("G29");
+        }
+    }
+}
